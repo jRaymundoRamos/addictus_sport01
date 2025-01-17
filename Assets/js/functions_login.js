00eb4d116823c2e054dac5ed4ad1cbb71936e924
RaymundoRms <jesus_ramos@proymoda.com>
2025-01-17 14:00:24 -0600

    ajustes de access y login

--- Assets/js/functions_login.js
+++ Assets/js/functions_login.js
@@ -30,8 +30,8 @@ document.addEventListener('DOMContentLoaded', function(){
 						var objData = JSON.parse(request.responseText);
 						if(objData.status)
 						{
-							//window.location = base_url+'/dashboard';
-							window.location.reload(false);
+							window.location = base_url+'/dashboard';
+							
 						}else{
 							swal("Atención", objData.msg, "error");
 							document.querySelector('#txtPassword').value = "";
